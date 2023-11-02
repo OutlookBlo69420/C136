5b8bceaeb94dfca4bc115ffbb14983db34523859
OutlookBlo69420 <141130754+OutlookBlo69420@users.noreply.github.com>
2023-11-02 20:04:20 +1100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+video = "";
+
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function setup()
+{
+    canvas = createCanvas(480, 380);
+    canvas.center();
+}
+
+
+function preload()
+{
+    video = createVideo('video.mp4');
+    video.hide();
+}
+
+function draw()
+{
+    image(video, 0, 0, 380, 380);
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
+
+function gotResult(error, results)
+{
+    if (error)
+    {
+        console.log(error)
+    }
+    console.log(results);
+    objects = results;
+}
